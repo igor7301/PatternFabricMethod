55ac6e712f7172ad9249dd6b616ffca1cf67ec55
igor7301 <igor7301@rambler.ru>
2014-12-31 14:31:54 +0700

    комментарии

--- Pattern_FabricMethod/src/pizzaStore/PizzaStore.java
+++ Pattern_FabricMethod/src/pizzaStore/PizzaStore.java
@@ -11,8 +11,11 @@ public abstract class PizzaStore {
 
     abstract Pizza fabricMethod_CreatePizza(PizzaType type);
 
+    //шаблонный метод orderPizza в котором указана логика работы
+    //преполагется что эта логика приготовления едина для всех видов пицц
     public Pizza orderPizza(PizzaType type) {
-        Pizza pizza = fabricMethod_CreatePizza(type);
+        Pizza pizza = fabricMethod_CreatePizza(type);//фабричный метод который определяется субклассами
+        //то есть контретные объекты создаются в субклассах
         pizza.bake();
         pizza.cut();
         pizza.box();
